759d8b191b3692464662a16a048d05fc3b6bb16e
TamimMahmud <tmahmud.jnubd@gmail.com>
2023-12-24 23:05:37 +0600

    puntuations remover

--- first-project/code_with_hary_practice/code_with_hary_practice/views.py
+++ first-project/code_with_hary_practice/code_with_hary_practice/views.py
@@ -13,7 +13,14 @@ def about(request):
     # get the text from form thar are sent by contact form
     formData = request.GET.get('text','default')
     print(formData)
-    return HttpResponse("This is tamim mahmud")
+    analyzedText=''
+    puntuations  = '''!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'''
+
+    for char in formData:
+        if char not in puntuations:
+            analyzedText+=char
+    params ={'data' : analyzedText}
+    return render(request,'analyzedText.html',params)
 def contact(request):
     
     return render(request,'index.html')
