34009efb95bb831d3fb4203c837d21af30349d0b
TamimMahmud <tmahmud.jnubd@gmail.com>
2023-12-25 13:26:28 +0600

    uppercase functionality added

--- first-project/code_with_hary_practice/code_with_hary_practice/views.py
+++ first-project/code_with_hary_practice/code_with_hary_practice/views.py
@@ -12,15 +12,24 @@ def index(request):
 def about(request):
     # get the text from form thar are sent by contact form
     formData = request.GET.get('text','default')
+    uppercase = request.GET.get('uppercase','off')
+    removePuntuations=request.GET.get('removePuntuations','off')
+
     print(formData)
+    print(removePuntuations)
     analyzedText=''
     puntuations  = '''!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'''
 
     for char in formData:
-        if char not in puntuations:
-            analyzedText+=char
+        if removePuntuations == 'on':
+            if char not in puntuations:
+                analyzedText+=char
+    for char in analyzedText:
+        if uppercase =='on':
+             analyzedText = analyzedText.upper()
     params ={'data' : analyzedText}
+    print(params)
     return render(request,'analyzedText.html',params)
-def contact(request):
     
+def contact(request):
     return render(request,'index.html')
